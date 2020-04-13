b60f47edc2fc05681f8f2c43b0391a982a13d044
Adrien Barbot <catzhead@hotmail.com>
2020-04-13 18:32:39 +0200

    corrected do root script

--- do/prepare_as_root.sh
+++ do/prepare_as_root.sh
@@ -10,12 +10,12 @@ usermod -aG sudo catzhead
 
 # docker (from DigitalOcean blog)
 
-apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt -qq install apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-apt update
+apt -qq update
 apt-cache policy docker-ce
-apt install docker-ce
+apt -qq install docker-ce
 usermod -aG docker catzhead
 systemctl status docker
 
