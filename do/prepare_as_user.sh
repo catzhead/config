e5d7270f2f17d2ef61411b3c1b3fe5cb3bb5a530
Adrien Barbot <catzhead@hotmail.com>
2020-04-13 15:25:48 +0200

    added .vimrc in do user script

--- do/prepare_as_user.sh
+++ do/prepare_as_user.sh
@@ -3,3 +3,4 @@
 # vim
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vimrc https://raw.githubusercontent.com/catzhead/config/master/arch-xps/home/.vimrc
