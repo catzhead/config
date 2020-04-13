24676e9c7ece26a4260ec586ee58fe3c069c661a
Adrien Barbot <catzhead@hotmail.com>
2020-04-13 18:31:10 +0200

    added docker to do root script

--- do/prepare_as_root.sh
+++ do/prepare_as_root.sh
@@ -6,7 +6,23 @@
 
 apt-get update
 apt-get -qq install sudo
-usermod -a -G sudo catzhead
+usermod -aG sudo catzhead
+
+# docker (from DigitalOcean blog)
+
+apt install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+usermod -aG docker catzhead
+systemctl status docker
+
+# docker-compose
+
+curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
 
 # install utils
 
